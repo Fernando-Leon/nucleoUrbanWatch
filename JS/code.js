1078bd51c55659e2b14ac99d4f76ca485801dd02
Fernando-Leon <leon.monroy.fernando@gmail.com>
2023-07-31 15:00:50 -0600

    Correcion 2

--- JS/code.js
+++ JS/code.js
@@ -1,50 +1,49 @@
-    // Variables globales para el mapa y el marcador del usuario
-    let map;
-    let userMarker;
+let map;
+let userMarker;
 
-    // Crea el evento para que cuando se cargue la página le pida acceso a la ubicación
-    window.addEventListener("load", () => {
-      if (navigator.geolocation) {
-        // El navegador admite geolocalización
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const userLocation = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude,
-            };
+// Crea el evento para que cuando se cargue la página le pida acceso a la ubicación
+window.addEventListener("load", () => {
+  if (navigator.geolocation) {
+    // El navegador admite geolocalización
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const userLocation = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
 
-            // Si el marcador ya existe, simplemente actualiza su posición
-            if (userMarker) {
-              userMarker.setPosition(userLocation);
-            } else {
-              // Si el marcador no existe, crea uno nuevo y llama a la función initMap
-              initMap(userLocation.lat, userLocation.lng);
-            }
-          },
-          (error) => {
-            console.error("Error al obtener la ubicación del usuario:", error);
-          }
-        );
-      } else {
-        console.error("El navegador no admite geolocalización.");
+        // Si el marcador ya existe, simplemente actualiza su posición
+        if (userMarker) {
+          userMarker.setPosition(userLocation);
+        } else {
+          // Si el marcador no existe, crea uno nuevo y llama a la función initMap
+          initMap(userLocation.lat, userLocation.lng);
+        }
+      },
+      (error) => {
+        console.error("Error al obtener la ubicación del usuario:", error);
       }
-    });
+    );
+  } else {
+    console.error("El navegador no admite geolocalización.");
+  }
+});
 
-    // Función para inicializar el mapa
-    function initMap(latitude, longitude) {
-      // Configura el centro del mapa en la ubicación proporcionada (latitude, longitude)
-      const initialLocation = { lat: latitude, lng: longitude };
+// Función para inicializar el mapa
+function initMap(latitude, longitude) {
+  // Configura el centro del mapa en la ubicación proporcionada (latitude, longitude)
+  const initialLocation = { lat: latitude, lng: longitude };
 
-      // Crea el objeto del mapa
-      map = new google.maps.Map(document.getElementById("map"), {
-        center: initialLocation,
-        zoom: 16, // Ajusta el nivel de zoom según tus necesidades
-      });
+  // Crea el objeto del mapa
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: initialLocation,
+    zoom: 16, // Ajusta el nivel de zoom según tus necesidades
+  });
 
-      // Crea un marcador en la ubicación del usuario
-      userMarker = new google.maps.Marker({
-        position: initialLocation,
-        map: map,
-        title: "Tu ubicación",
-      });
-    }
+  // Crea un marcador en la ubicación del usuario
+  userMarker = new google.maps.Marker({
+    position: initialLocation,
+    map: map,
+    title: "Tu ubicación",
+  });
+}
