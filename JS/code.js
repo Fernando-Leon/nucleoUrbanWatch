6b077c553f608d1d65f8ef562b9d1cd65d87e4a1
Fernando-Leon <leon.monroy.fernando@gmail.com>
2023-07-31 14:24:45 -0600

    Position user

--- JS/code.js
+++ JS/code.js
@@ -1,58 +1,53 @@
-var userLocation = {lat: 40.7128, lng: -74.0060 };
+// Variables globales para la ubicación y el marcador del usuario
+let userLocation;
+let userMarker;
 
-// Crea el evento para que cuando se cargue la pagina le pida acceso a la ubicacion
+// Función para inicializar el mapa
+function initMap(latitude, longitude) {
+  // Configura el centro del mapa en la ubicación proporcionada (latitude, longitude)
+  const initialLocation = { lat: latitude, lng: longitude };
+
+  // Crea el objeto del mapa
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: initialLocation,
+    zoom: 16, // Ajusta el nivel de zoom según tus necesidades
+  });
+
+  // Crea un marcador en la ubicación del usuario
+  userMarker = new google.maps.Marker({
+    position: initialLocation,
+    map: map,
+    title: "Tu ubicación",
+  });
+}
+
+// Crea el evento para que cuando se cargue la página le pida acceso a la ubicación
 window.addEventListener("load", () => {
   if (navigator.geolocation) {
     // El navegador admite geolocalización
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        // La ubicación del usuario se obtuvo correctamente
-        const userLocation = {
+        // ...
+        userLocation = {
           lat: position.coords.latitude,
           lng: position.coords.longitude,
         };
 
-        // Centra el mapa en la ubicación del usuario
-        initMap( userLocation.lat, userLocation.lng );
-        
-        // Aquí puedes realizar cualquier otra lógica relacionada con la ubicación del usuario
-        // Por ejemplo, puedes mostrar la distancia o tiempo de llegada a los vehículos cercanos, etc.
+        // Si el marcador ya existe, simplemente actualiza su posición
+        if (userMarker) {
+          userMarker.setPosition(userLocation);
+        } else {
+          // Si el marcador no existe, crea uno nuevo y llama a la función initMap
+          initMap(userLocation.lat, userLocation.lng);
+        }
       },
       (error) => {
         // Ocurrió un error al obtener la ubicación del usuario
         console.error("Error al obtener la ubicación del usuario:", error);
         // Aquí puedes manejar el error, por ejemplo, mostrando un mensaje al usuario
       }
-      );
+    );
   } else {
     console.error("El navegador no admite geolocalización.");
   }
 });
-
-
-
-
-// Función para inicializar el mapa
-function initMap( latitude, longitude ) {
-  // Configura el centro del mapa en una ubicación inicial
-  const initialLocation = { lat: latitude, lng: longitude };
-  
-  // Crea el objeto del mapa
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: initialLocation,
-    zoom: 10, // Ajusta el nivel de zoom según tus necesidades
-  });
-  
-  map.setCenter(userLocation);
-  // Lógica para obtener la ubicación de los vehículos de transporte público (puedes usar datos de ejemplo o una API real aquí)
-
-  // Lógica para mostrar los marcadores de los vehículos de transporte público
-  // Ejemplo de marcador:
-  // const vehicleMarker = new google.maps.Marker({
-  //   position: { lat: vehicleLat, lng: vehicleLng },
-  //   map: map,
-  //   title: "Nombre del vehículo",
-  // });
-
-  // Lógica para calcular rutas y tiempo de llegada (puedes usar datos de ejemplo o una API real aquí)
-}
